9a909320fb51a7f09c7461a0e77de02fe6c9a455
Victor Nițu <victor@thesponge.eu>
2016-01-02 20:27:27 +0200

    CORS workaround

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
 
+  match '*any' => 'application#options', :via => [:options]
+
   defaults format: "json" do
-    devise_for :users, :controllers => {sessions: 'sessions', registrations: 'registrations',
-      confirmations: 'confirmations', passwords: 'passwords'}
+    #devise_for :users, :controllers => {sessions: 'sessions', registrations: 'registrations',
+    #  confirmations: 'confirmations', passwords: 'passwords'}
 
     resources :users, only: [:show]
 
@@ -13,10 +15,10 @@ Rails.application.routes.draw do
 
         namespace :elastic do
           get 'contracts', to: 'contracts#index'
-          get 'contracts/:id', to: 'contracts#show'
           get 'contracts/count', to: 'contracts#count'
           # Aggregation of document countries
           get 'contracts/countries', to: 'countries#index'
+          get 'contracts/:id', to: 'contracts#show'
           # Query cpvs for autocompletion
           get 'cpvs/autocomplete', to: 'cpvs#autocomplete'
         end
